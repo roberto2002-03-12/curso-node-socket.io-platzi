335ead8cd278786f296784caac4c65e19bb83d98
roberto2002-03-12 <robertog-18@hotmail.com>
2024-08-15 07:53:15 -0500

    bases de namespaces

--- src/index.ts
+++ src/index.ts
@@ -9,51 +9,28 @@ const io = new Server(httpServer)
 
 app.use(express.static(path.join(__dirname, 'views')))
 
-const socketsOnline: string[] = [];
-
 app.get("/", (req, res) => {
   res.sendFile(__filename + "/views/index.html")
 })
-// on: declarar eventos que se pueden ejecutar varias veces
-io.on('connection', socket => {
-  // console.log(`Clientes conectadois: ${io.engine.clientsCount}`)
-  // console.log(`ID socket conectado: ${socket.id}`)
 
-  // socket.on('disconnect', () => {
-  //   console.log(`El socket ${socket.id} se ha desconectado`)
-  // })
+// namespace general por defecto 
+// io.on('connection', socket => {
 
-  // socket.conn.once('upgrade', () => {
-  //   console.log(`Se ha pasa la conexión de HTTP Long-Polling a ${socket.conn.transport.name}`)
-  // })
+// })
 
-  socketsOnline.push(socket.id)
+// namespace especifico
 
-  // emitir evento custom
-  socket.emit('welcome', 'Conexión recibida')
+// namespace: teachers
+const teachers = io.of('teachers')
+// namespace: students
+const students = io.of('students')
 
-  // con on recibes eventos con emit emites eventos
-  socket.on('server-client', data => {
-    console.log(data)
-  })
+teachers.on('connection', socket => {
+  console.log(`${socket.id} se ha conectado a teachers`)
+})
 
-  io.emit('emit-to-everyone', `${socket.id} se ha conectado`)
-
-  socket.on('emit-to-last-connected', message => {
-    const lastClientConnected = socketsOnline[socketsOnline.length - 1]
-    // enviar eventos a clientes especificos con su id
-    io.to(lastClientConnected).emit('saludos', message)
-  })
-
-  socket.emit('off', 'holi')
-  setTimeout(() => {
-    socket.emit('off', 'holi')
-  }, 3000);
+students.on('connection', socket => {
+  console.log(`${socket.id} se ha conectado a students`)
 })
 
 httpServer.listen(3000)
-
-/*
-connection, disconnect y upgrade son eventos declarados como base
-
-*/
--- src/views/js/index.js
+++ src/views/js/index.js
@@ -1,75 +1,24 @@
-// Socket.io para el lado del cliente
-const socket = io()
+// no puedes hacer esto porque te unes al namespace general,
+// cosa que para este caso no se quiere realizar esto
+// const socket = io()
 
-function checkSocketStatus() {
-  console.log(`Estado del socket: ${socket.connected}`)
+const user = prompt('Escribe tu usuario')
+
+const teachers = ['Eduardo', 'Midu', 'Jason']
+
+let socketNamespace, group
+
+const chat = document.querySelector('#chat')
+const namespace = document.querySelector('#namespace')
+
+if(teachers.includes(user)) {
+  socketNamespace = io('/teachers')
+  group = 'teachers'
+} else {
+  socketNamespace = io('/students')
+  group = 'students'
 }
 
-// estoy atrapando eventos que envia el servidor y ejecutar funciones según el evento
-// socket.on('connect', () => {
-//   console.log(`El socket se ha conectado: ${socket.id}`)
-//   checkSocketStatus()
-// })
-
-// esto solo funciona si muere el servidor
-// socket.on('disconnect', () => {
-//   checkSocketStatus()
-//   console.log(`El socket se ha desconectado: ${socket.id}`)
-// })
-
-// socket.io.on('reconnect_attempt', () => {
-//   console.log('Intento de conexión al servidor')
-// })
-
-// socket.io.on('reconnect', () => {
-//   console.log('Conexión recuperada')
-// })
-
-// socket.io.on('connect_error', () => {
-//   console.log('Falla en el intento de reconexión')
-// })
-
-// Recibir evento custom de servidor
-socket.on('welcome', data => {
-  const text = document.querySelector('#text')
-  text.textContent = data
+socketNamespace.on('connect', () => {
+  namespace.textContent = group
 })
-
-const emitToServer = document.querySelector('#emit-to-server')
-emitToServer.addEventListener('click', () => {
-  socket.emit('server-client', 'Se esta enviando un evento desde el cliente al servidor')
-})
-
-socket.on('emit-to-everyone', data => {
-  const text = document.querySelector('#id-conn')
-  text.textContent = data
-})
-
-const emitToLastConnected = document.querySelector('#emit-to-last-connected')
-emitToLastConnected.addEventListener('click', () => {
-  socket.emit('emit-to-last-connected', 'Evento a la última conexión')
-})
-
-socket.on('saludos', message => {
-  console.log(message)
-})
-
-// socket.on: recibir eventos
-// socket.one: solo recibe un evento en especifico una sola vez
-// socket.off: apaga eventos para que el cliente o servidor ya deje de ejecutarlos
-
-const listener = () => {
-  console.log('Se apaga el evento')
-}
-
-socket.on('off', listener)
-
-setTimeout(() => {
-  // debes usar funciones con nombres no anonimas porque el off necesita especificación
-  // para poder apagar dicho evento
-  socket.off('off', listener)
-}, 2000)
-
-// socket.on: recibir eventos
-// socket.one: solo recibe un evento en especifico una sola vez
-// socket.off: apaga eventos para que el cliente o servidor ya deje de ejecutarlos
